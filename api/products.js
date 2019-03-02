7d5593d90852367af644b207221dd1bd90f542d1
Aprilia-Hartami <apriliahartami@gmail.com>
2019-03-02 14:33:29 +0200

    updated product data

--- api/products.js
+++ api/products.js
@@ -8,30 +8,60 @@ router.get("/", function(req, res, next) {
       id: 1,
       productname: "Wanderlust",
       sourcepic: "http://ahartami.files.wordpress.com/2019/02/wanderlust.png",
-      color: "green, yellow, brown",
-      price: "€ 120"
+      description:
+        "This is a watercolor painting on paper which tells about a strong desire to explore the world.",
+      color: "green, yellow",
+      price: "€ 120",
+      contact: +491792811999
     },
     {
       id: 2,
       productname: "Phinisi Boat",
       sourcepic: "https://ahartami.files.wordpress.com/2019/02/cam02999.jpg",
+      description:
+        "This is an acrylic painting on paper. It is about a traditional boat which is used by people for centuries to sail Pacific and Hindian Ocean.",
       color: "red, blue",
-      price: "€ 140"
+      price: "€ 140",
+      contact: +49281373829
     },
     {
       id: 3,
       productname: "Stay Weird",
       sourcepic: "https://ahartami.files.wordpress.com/2019/02/stay-weird.png",
+      description:
+        "This is a watercolor painting on paper. It is a motivation to be brave in differences.",
       color: "brown",
-      price: "€ 90"
+      price: "€ 90",
+      contact: +35827672818
     },
     {
       id: 4,
       productname: "Anything is possible!",
       sourcepic:
         "https://ahartami.files.wordpress.com/2019/02/17855553_10212807683894784_2848170274801137961_o.jpg",
+      description:
+        "This is a watercolor painting on paper. It is a motivation to try something although it is a difficult things.",
       color: "blue",
-      price: "€ 90"
+      price: "€ 90",
+      contact: +62813749402
+    },
+    {
+      id: 5,
+      productname: "Under my Umbrella",
+      sourcepic: "https://ahartami.files.wordpress.com/2019/02/cam03044.jpg",
+      description: "This is an acrylic painting on paper.",
+      color: "red, blue",
+      price: "€ 100",
+      contact: +35827672818
+    },
+    {
+      id: 6,
+      productname: "Carpe Diem",
+      sourcepic: "https://ahartami.files.wordpress.com/2019/02/cam03039.jpg",
+      description: "This is a watercolor painting on paper.",
+      color: "grey",
+      price: "€ 90",
+      contact: +62813749402
     }
   ]);
 });
